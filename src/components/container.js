1704163596d8d04ba5c7748c56efea720d5a4694
Paulo Raimundo <paulo.raimundo@accountfy.com>
2024-02-03 13:20:21 -0300

    feat: Criando o componente de GRID

---
+++ src/components/container.js
@@ -0,0 +1,13 @@
+import { twMerge } from 'tailwind-merge';
+
+export function ContainerGrid({ children, className }) {
+
+    const defaultClass = 'w-full max-w-grid mx-auto px-3';
+    const conbinedClasses = twMerge(defaultClass, className);
+
+    return (
+        <div className={conbinedClasses}>
+            {children}
+        </div>
+    )
+}
